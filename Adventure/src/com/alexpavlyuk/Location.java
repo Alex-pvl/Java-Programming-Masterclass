bfdea35ab341960a0d30fee559009185f89c44fe
Alexandr Pavlyuk <sasha.pavlyuk.1996@inbox.ru>
2022-01-27 01:48:24 +0700

    27.01.22

---
+++ Adventure/src/com/alexpavlyuk/Location.java
@@ -0,0 +1,73 @@
+package com.alexpavlyuk;
+
+import java.util.*;
+
+public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description) {
+        this.locationID = locationID;
+        this.description = description;
+        exits = new HashMap<>();
+        exits.put("Q", 0);
+    }
+
+    public void addExit(String direction, int location) {
+        exits.put(direction, location);
+    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<>(exits);
+    }
+}
+
+/*  Ex#51
+    Edit the code by following the steps in the description
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+public final class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description, Map<String, Integer> exits) {
+        this.locationID = locationID;
+        this.description = description;
+        if (exits != null) {
+            this.exits = new HashMap<String, Integer>(exits);
+        } else {
+            this.exits = new HashMap<String, Integer>();
+        }
+        this.exits.put("Q", 0);
+    }
+
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<String, Integer>(exits);
+    }
+
+}
+*/
+
